c92b7f85c9979111b2021646d8da71f7252912e1
Sébastien <sebastien.schlichting@hotmail.com>
2026-03-04 13:36:05 +0100

    Changed logic to use posix and not console input for better ux

--- python-http-browser/browser.py
+++ python-http-browser/browser.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
+import argparse
 
 # --headless damit kein firefox fenster geöffnet wird und alles in der Konsole ausgegeben wird
 options = FirefoxOptions()
@@ -13,20 +14,23 @@ service = FirefoxService(
     service_args=['--log', 'trace'],
 )
 
+# Damit --parsing funktioniert, posix für gutes ux
+parser = argparse.ArgumentParser()
+parser.add_argument('--url')
+parser.add_argument('--tag')
+args = parser.parse_args()
+
 driver = webdriver.Firefox(options=options, service=service)
+driver.get(args.url)
 
-# user kann URL eingeben und driver holt sie
-url = input('Enter URL: ')
-driver.get(url)
-
-page_title = driver.title
-print("Message: ", page_title)
-
-# für später: definieren welcher tag gesucht werden soll
-# tag_name = input('Enter Tag: ')
-# header = driver.find_element(by=By.TAG_NAME, value=tag_name)
-
+# Selenium Zeit geben das Element zu finden
 driver.implicitly_wait(10)
 
-input('Press Enter to quit...')
+# wir holen den Tag mit dem --tag aus dem Argument
+tag_output = driver.find_element(by=By.TAG_NAME, value=args.tag)
+
+# Ohne .text wird nur das Web Element ausgegeben
+print(f"\033[33mOutput tag:\033[0m", tag_output.text)
+
+# Driver beenden nach dem scraping
 driver.quit()
