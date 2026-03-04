64c7ed3b40be8535385d6336673482ad43da9249
Sébastien <sebastien.schlichting@hotmail.com>
2026-03-04 16:04:23 +0100

    Add the list-cookies command

--- python-http-browser/browser.py
+++ python-http-browser/browser.py
@@ -10,7 +10,7 @@ options.add_argument('--headless')
 # firefox json view ausblenden, da dieser die Formatierung verhindert
 options.set_preference("devtools.jsonview.enabled", False)
 
-# Da Firefox durchgehend abgebrochen wird, logge ich den ouput
+# Ich logge den output in error.log
 service = FirefoxService(
     log_output=open('error.log', 'w'),
     service_args=['--log', 'trace'],
@@ -35,6 +35,10 @@ post_parser = subparser.add_parser('post')
 post_parser.add_argument('--url')
 post_parser.add_argument('--param', action='append')
 
+# Cookie Liste
+cookies = subparser.add_parser('list-cookies')
+cookies.add_argument('--url')
+
 args = parser.parse_args()
 
 driver = webdriver.Firefox(options=options, service=service)
@@ -44,11 +48,11 @@ output = None
 
 if args.command == 'scrape': # Hier scrapen wir nach Tag
     driver.get(args.url)
-    output = driver.find_element(by=By.TAG_NAME, value=args.tag) # tag in value einsetzen
+    output = driver.find_element(by=By.TAG_NAME, value=args.tag).text # ohne .text wird nur das WebElement ausgegeben
 
 elif args.command == 'get': # Hier verwenden wit get und fügen parameter hinzu
     driver.get(args.url + '?' + args.param)
-    output = driver.find_element(by=By.TAG_NAME, value='body')
+    output = driver.find_element(by=By.TAG_NAME, value='body').text
 
 elif args.command == 'post':
     driver.get(args.url)
@@ -62,11 +66,16 @@ elif args.command == 'post':
     # Auf den Form button klicken
     submit_button = driver.find_element(by=By.TAG_NAME, value='button')
     submit_button.click()
-    output = driver.find_element(by=By.TAG_NAME, value='body')
+    output = driver.find_element(by=By.TAG_NAME, value='body').text
+
+elif args.command == 'list-cookies':
+    driver.get(args.url)
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+        output = '\n'.join([f"\033[32m{c['name']}\033[0m : {c['value']}" for c in cookies])
 
 
-# .text extrahiert den Text aus dem WebElement
-print(f"\033[33mOutput:\033[0m", output.text)
+print(f"\033[1m\033[34mOUTPUT:\033[0m\n{output if output else 'No output'}")
 
 # Driver beenden nach dem scraping
 driver.quit()
